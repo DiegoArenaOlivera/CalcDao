3d756c1220a890691fef6abe2b5848f55750022c
Viry Gomez <vgomez@codigodelsur.com>
2016-02-24 11:08:51 -0300

    update

---
+++ calcDao.framework/Headers/FClass.h
@@ -0,0 +1,17 @@
+//
+//  FClass.h
+//  NewFramework
+//
+//  Created by Viry Gomez on 2/19/16.
+//  Copyright © 2016 CodigoDelSur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FClass : UIViewController
+
++ (void)printOnConsole;
+
+- (void)showCamera;
+
+@end
